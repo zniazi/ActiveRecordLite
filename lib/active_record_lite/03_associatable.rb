e89e535f3b7289d487fa7baa2f2e3fd397b72d21
Zak Niazi <zniazi1029@gmail.com>
2014-10-03 20:08:09 -0400

    Add has_many through associatable methods.

--- lib/active_record_lite/03_associatable.rb
+++ lib/active_record_lite/03_associatable.rb
@@ -10,11 +10,11 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
@@ -32,25 +32,53 @@ end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{self_class_name.underscore}_id".to_sym,
+      class_name: name.to_s.singularize.camelcase,
+      primary_key: :id
+    }
+    
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 module Associatable
-  # Phase IVb
   def belongs_to(name, options = {})
-    # ...
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+    
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      
+      key_val = self.send(options.foreign_key)
+      options
+          .model_class
+          .where(options.primary_key => key_val)
+          .first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] =
+      HasManyOptions.new(name, self.name, options)
+      
+      define_method(name) do
+        options = self.class.assoc_options[name]
+        
+        key_val = self.send(options.primary_key)
+        options
+            .model_class
+            .where(options.foreign_key => key_val)
+      end
   end
 
   def assoc_options
-    # Wait to implement this in Phase V. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
